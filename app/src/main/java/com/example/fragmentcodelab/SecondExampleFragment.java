7aecbcafdf7a1b39b9c85ffe86aee25f051159d1
AllTeach <33581448+AllTeach@users.noreply.github.com>
2022-07-16 22:32:37 +0300

    Added interface for data

--- app/src/main/java/com/example/fragmentcodelab/SecondExampleFragment.java
+++ app/src/main/java/com/example/fragmentcodelab/SecondExampleFragment.java
@@ -2,17 +2,27 @@ package com.example.fragmentcodelab;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SecondExampleFragment extends Fragment {
 
 
+    private RegisterResult registerResult;
+    private Button b;
+    private EditText etMail,etPassword;
+    public interface RegisterResult {
+        void dataFromRegister(String mail,String password);
+
+    }
     public SecondExampleFragment() {
         // Required empty public constructor
     }
@@ -31,7 +41,33 @@ public class SecondExampleFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_second_example, container, false);
 
+        etMail = view.findViewById(R.id.frgusername);
+        etPassword = view.findViewById(R.id.frgpassword);
+        b= view.findViewById(R.id.frgBtnlogin);
 
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etMail.getText().toString();
+                String password = etPassword.getText().toString();
+                // pass data to activity
+                if(registerResult!=null)
+                    registerResult.dataFromRegister(email,password);
+            }
+        });
         return view;
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(getActivity() instanceof RegisterResult)
+            this.registerResult = (RegisterResult) getActivity();
+    }
+
+
 }
--- app/src/main/java/com/example/fragmentcodelab/TabFragmentActivity.java
+++ app/src/main/java/com/example/fragmentcodelab/TabFragmentActivity.java
@@ -6,12 +6,15 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class TabFragmentActivity extends FragmentActivity {
+public class TabFragmentActivity extends FragmentActivity implements SecondExampleFragment.RegisterResult{
+    private static final String TAG = "TabFragment Activity";
     private ViewPager2 viewPager;
     private DemoFragmentPagerAdapter pagerAdapter;
     private TabLayout tabLayout;
@@ -22,10 +25,6 @@ public class TabFragmentActivity extends FragmentActivity {
         // initiating the tabhost
         viewPager = findViewById(R.id.viewPager);
         TabLayout tabLayout = findViewById(R.id.tab_layout);
-
-
-
-
         DemoFragmentPagerAdapter
                 adapter
                 = new DemoFragmentPagerAdapter(this);
@@ -36,6 +35,12 @@ public class TabFragmentActivity extends FragmentActivity {
         new TabLayoutMediator(tabLayout,viewPager,(tab, position) -> tab.setText("TAB " + (position + 1))
         ).attach();
 
+    }
+
+    @Override
+    public void dataFromRegister(String mail, String password) {
+        Log.d(TAG, "dataFromRegister: recevied in Activity " + mail + " , " + password);
+        Toast.makeText(this, "Received data in Activity " + mail + " , " + password, Toast.LENGTH_SHORT).show();
 
     }
 }
